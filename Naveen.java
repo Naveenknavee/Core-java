14d39ec6fdb147a3d7b4aa0d4cf328363fc9a6d9
NAVEEN KUMAR <93428877+Naveenknavee@users.noreply.github.com>
2021-11-24 16:20:14 +0530

    Day 1

---
+++ Naveen.java
@@ -0,0 +1,10 @@
+package tamilnadu.madurai;
+public class Naveen
+{
+public static void main (String[]args)
+{
+System.out.println("Company in madurai");
+Company com=new Company();
+com.providSnacks();
+}
+}
